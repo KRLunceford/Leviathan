13c7d063bc8d6b4f3e2fef3af7e59ec5e243e5ac
unknown <cvinson@knights.ucf.edu>
2015-04-30 18:00:31 -0400

    'update'

--- config/env/all.js
+++ config/env/all.js
@@ -56,7 +56,7 @@ module.exports = {
 				'public/lib/angular-socket-io/socket.js',
 				'public/lib/bootstrap/js/dropdown.js',
 				'public/lib/angular/angular.js',
-				'public/lib/angular-resource/angular-resource.js',
+				//'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-animate/angular-animate.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
